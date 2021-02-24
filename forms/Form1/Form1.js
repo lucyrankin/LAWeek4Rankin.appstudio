2b8c34bc950bfcef35cd597725a78622a5e938f8
lucyrankin <79422779+lucyrankin@users.noreply.github.com>
2021-02-24 15:06:07 -0600

    Initial commit

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,6 @@
+var petNames = ["Darcy","Jack","Jerry","Marty","Vito","Vinny","Riley","Saddie","Marge","Sam"]
+var petNames = withSal
+petNames.push("Sal")
+console.log('The third pets name is' + ' 'petNames[2])
+let lengthpetNames = petNames.length 
+console.log('The arrary has + ' ' +lengthpetName + ' ' + names in it')
